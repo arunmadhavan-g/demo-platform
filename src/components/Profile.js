a5dc5135dda578db953f62823aa07858f346c165
arunmadhavan-g <arunmadhavan.g@gmail.com>
2019-11-10 23:30:09 +0530

    [Arun] | Adds links for social accounts

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,26 @@
 import React from "react"
 import { Header, Image } from "semantic-ui-react"
 import { Icon } from "semantic-ui-react"
+import { Link } from "gatsby"
 import styles from "../styles/Profile.module.scss"
 
+const socials = [
+  { icon: "twitter", url: "https://twitter.com/ArunmadhavanG" },
+  { icon: "linkedin", url: "https://www.linkedin.com/in/arunmadhavang" },
+  { icon: "github", url: "https://github.com/arunmadhavan-g" },
+]
 const Profile = () => (
   <div className={styles.container}>
     <div>
       <Header as="h4">A Blog from Arun Madhavan</Header>
       <div className={styles.social}>
-        <Icon name="twitter" size="large" />
-        <Icon name="linkedin" size="large" />
-        <Icon name="github" size="large" />
+        {socials.map(x => {
+          return (
+            <a href={x.url}>
+              <Icon name={x.icon} size="large" />
+            </a>
+          )
+        })}
       </div>
     </div>
     <Image
