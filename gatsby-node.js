0f6277ad8ce95e95c6950c44d421fdfa57d7625d
Govindarajan <marun12@MGC12FT1C3MD6N.chennaigtbc.ford.com>
2021-11-14 00:02:51 +0530

    Arun| Handles exposing profile data as node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,59 @@
-const _ = require("lodash")
+const {camelCase} = require("lodash")
+const { Octokit } = require("@octokit/rest");
+const frontmatterParser = require('@github-docs/frontmatter')
+const renderContent = require('@github-docs/render-content')
 
-exports.createPages = async ({ graphql, actions: { createPage } }) =>
+const octokit = new Octokit({ 
+  auth: `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`,
+});
+
+const downloadContent = async path => {
+  const {data} =  (await octokit.rest.repos.getContent({
+                    owner: "arunmadhavan-g",
+                    repo: "Profile",
+                    path: path
+                  }));
+              
+  return Buffer.from(data.content, 'base64').toString('utf-8');
+}
+
+exports.sourceNodes = async ({node, actions, createNodeId, createContentDigest}, pluginOptions) => {
+  const {createNode} = actions
+
+  const {data} = await octokit.request("GET /repos/{owner}/{repo}/contents", {
+    owner: "arunmadhavan-g",
+    repo: "Profile",
+    path: "/"
+  });
+
+  (await Promise.all(data
+  .filter(x => x.name.includes("Project"))
+  .map(async x => ({...x, content: (await downloadContent(x.path))}))
+  .map(async x => {
+    const { data, content } = frontmatterParser((await x).content);
+    return ({
+    id: createNodeId(`${x.name}-repoRecords`),
+          children: [],
+          internal: {
+              type: `repoRecords`,
+          },
+          pageInfo: {
+            ...x, 
+            htmlContent: await renderContent(content, {}),
+            frontmatter: data,
+          },
+    })
+  })))
+  .forEach(x => {
+    createNode({...x, internal:{...x.internal, contentDigest: createContentDigest(x)}})
+  })
+  
+};
+
+
+
+exports.createPages = async ({ graphql, actions: { createPage } }) =>{
+
   (await graphql(`
   query MyQuery {
     allMultiGitSource {
@@ -20,8 +73,10 @@ exports.createPages = async ({ graphql, actions: { createPage } }) =>
     }
   }`)).data.allMultiGitSource.edges.map(x => x.node.pageInfo).forEach(page => {
     createPage({
-      path: `/${_.camelCase(page.title)}`,
+      path: `/${camelCase(page.title)}`,
       component: require.resolve("./src/templates/page-template.js"),
       context: { page },
     })
-  })
+  });
+
+}
